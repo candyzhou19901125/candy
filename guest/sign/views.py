8ac2e3bcc021a1600f75127616eeb153906878d9
candyzhou19901125 <3243870281@qq.com>
2017-07-28 17:20:42 +0800

    Update views.py

--- guest/sign/views.py
+++ guest/sign/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponseRedirect
 from django.contrib import auth
 from sign.models import Event, Guest
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import  Paginator, EmptyPage, PageNotAnInteger
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
@@ -85,6 +86,43 @@ def search_phone(request):
         # 如果page不在范围，取最后一页面
         contacts = paginator.page(paginator.num_pages)
     return render(request, "guest_manage.html", {"user": username, "guests": contacts})
+
+
+# 签到页面
+@login_required
+def sign_index(request, eid):
+    event = get_object_or_404(Event, id=eid)
+    return render(request, 'sign_index.html', {'event': event})
+
+
+# 签到动作
+@login_required
+def sign_index_action(request, eid):
+    event = get_object_or_404(Event, id=eid)
+    phone = request.POST.get('phone', '')
+    print(phone)
+    result = Guest.objects.filter(phone=phone)
+    if not result:
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'phone error.'})
+    result = Guest.objects.filter(phone=phone, event_id=eid)
+    if not result:
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'event id or phone error.'})
+    result = Guest.objects.get(phone=phone, event_id=eid)
+    if result.sign:
+        return render(request,'sign_index.html',{'event':event, 'hint': 'user has sign in.'})
+    else:
+        Guest.objects.filter(phone=phone, event_id =eid).update(sign='1')
+        return render(request, 'sign_index.html',{'event':event,  'hint': 'sign in success!', 'guest': result})
+
+
+# 退出登录
+@login_required
+def logout(request):
+    auth.logout(request)  # 退出登录
+    response = HttpResponseRedirect('/index/')
+    return response
+
+
 
 
 
