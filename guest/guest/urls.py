9925a32e3dae986230f2d098cfa4ad71acda46c6
candyzhou19901125 <3243870281@qq.com>
2017-07-28 17:22:14 +0800

    Update urls.py

--- guest/guest/urls.py
+++ guest/guest/urls.py
@@ -30,4 +30,6 @@ urlpatterns = [
     url(r'^search_phone/$', views.search_phone),
     url(r'^guest_manage/$', views.guest_manage),
     url(r'^sign_index/(?P<eid>[0-9]+)/$', views.sign_index),
+    url(r'^sign_index_action/(?P<eid>[0-9]+)/$', views.sign_index_action),
+    url(r'^logout/$', views.logout),
 ]
